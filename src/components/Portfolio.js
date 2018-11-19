37f69dce9fb25b75dc6e22c4ad7b12ba92a7d03f
Shelby Inloes <shelbyinloes@gmail.com>
2018-11-18 20:21:18 -0600

    adding portfolio content

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,16 @@
 import React, {Component} from "react";
 import {Row, Col} from "react-bootstrap";
+import Coverflow from "react-coverflow";
+import { StyleRoot } from "radium";
+import portCalc from "../images/portCalc.png";
+import portPass from "../images/portPass.png";
+import portTic from "../images/portTic.png";
+
+
 
 const portfolio = {
-  height: "700px"
+  height: "700px",
+  backgroundColor: "white"
 }
 const projectDescription = {
   backgroundColor: "rgba(226, 152, 74, 0.85)", 
@@ -19,10 +27,28 @@ class Portfolio extends Component {
   render(){
     return(
       <div>
-        <Row style={portfolio}>
-          <Col style={projectDescription} sm={7}>Porfolio description</Col>
-          <Col style={projects} sm={5}>Project carousel</Col>
-        </Row>
+        <StyleRoot>
+          <Coverflow
+            displayQuantityOfSide={2}
+            navigation
+            infiniteScroll
+            enableHeading
+            media={{
+              '@media (max-width: 900px)': {
+                width: '600px',
+                height: '300px'
+              },
+              '@media (min-width: 900px)': {
+                width: '960px',
+                height: '600px'
+              }
+            }}
+          >
+            <img src={portTic} alt={"Tic-Tac-Toe"} data-action="https://shelbyinloes.github.io/web-workbook/08week/tic-tac-toe/index.html" />
+            <img src={portPass} alt={"Password Strength Meter"} data-action="https://codepen.io/shelbyinloes/pen/zpzQMr" />
+            <img src={portCalc} alt={"Simple Calculator"} data-action="https://codepen.io/shelbyinloes/pen/qVXPpL?editors=1010" />
+          </Coverflow>
+        </StyleRoot>
       </div>
     )}
 } 
