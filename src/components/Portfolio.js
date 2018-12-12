dfce6f6a3ce823503161e1b5a48584ef3303f8e8
Shelby Inloes <shelbyinloes@gmail.com>
2018-12-12 14:21:45 -0600

    gh holder until portfolio projects are up

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,18 +2,22 @@ import React, {Component} from "react";
 // import {Row, Col} from "react-bootstrap";
 import Coverflow from "react-coverflow";
 import { StyleRoot } from "radium";
-import portCalc from "../images/portCalc.png";
-import portPass from "../images/portPass.png";
-import portTic from "../images/portTic.png";
+// import portCalc from "../images/portCalc.png";
+// import portPass from "../images/portPass.png";
+// import portTic from "../images/portTic.png";
+import portGit from "../images/portGit.png";
 
-
-
+const portTitle = {
+  textAlign: "center",
+  color: "#00B2B8"
+}
 // this section will have a carousel-type motion to scroll through various projects  
 
 class Portfolio extends Component {
   render(){
     return(
       <div>
+        <h1 style={portTitle}>Working on something special for my porfolio page. In the meantime, checkout my GitHub page!</h1>
         <StyleRoot>
           <Coverflow
             displayQuantityOfSide={2}
@@ -31,9 +35,9 @@ class Portfolio extends Component {
               }
             }}
           >
-            <img src={portTic} alt={"Tic-Tac-Toe"} data-action="https://shelbyinloes.github.io/web-workbook/08week/tic-tac-toe/index.html" />
-            <img src={portPass} alt={"Password Strength Meter"} data-action="https://codepen.io/shelbyinloes/pen/zpzQMr" />
-            <img src={portCalc} alt={"Simple Calculator"} data-action="https://codepen.io/shelbyinloes/pen/qVXPpL?editors=1010" />
+            <img src={portGit} alt={"Shelby's GitHub"} data-action="https://github.com/shelbyinloes" />
+            {/* <img src={portPass} alt={"Password Strength Meter"} data-action="https://codepen.io/shelbyinloes/pen/zpzQMr" /> */}
+            {/* <img src={portCalc} alt={"Simple Calculator"} data-action="https://codepen.io/shelbyinloes/pen/qVXPpL?editors=1010" /> */}
           </Coverflow>
         </StyleRoot>
       </div>
